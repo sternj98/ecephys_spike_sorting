28867bec9f48bf8c01caa48cc01b88616970056f
xy103 <xintong.cindy.yuan@gmail.com>
2022-03-09 13:18:42 -0500

    added probe ID to dj_ecephys_update

--- ecephys_spike_sorting/scripts/dj_ecephys_update.py
+++ ecephys_spike_sorting/scripts/dj_ecephys_update.py
@@ -48,7 +48,12 @@ for this_key,this_unprocessed_np_path in zip(session_keys,ephys_paths):
         session_name_gx_tx = [x for x in os.listdir(npx_directory_full) if x[-4:] == ".bin" ][0].split('.')[0]
         session_name = session_name_gx_tx[:session_name_gx_tx.index('_g')]
         gate = session_name_gx_tx[session_name_gx_tx.index('_g')+2]
-        these_recording_specs = [session_name, gate, 'start,end','0', ['thalamus'] ]
+        probe_idx = [x[-1] for x in os.listdir(npx_directory_full) if x[-5:-1] == "imec" ]
+        if len(probe_idx)>1:
+            all_probes = ", ".join(probe_idx)
+        else:
+            all_probes = probe_idx[0]
+        these_recording_specs = [session_name, gate, 'start,end',all_probes, ['thalamus'] ]
         npx_directory_split = this_unprocessed_np_path.split(os.sep)
         catGT_dest = os.path.join(results_directory,npx_directory_split[-4],npx_directory_split[-3],npx_directory_split[-2],npx_directory_split[-1])
         output_dest = os.path.join(catGT_dest,'catGT_' + these_recording_specs[0] + '_g' + these_recording_specs[1])
